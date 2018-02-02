a963eb861a363e8f48c6b665638ed01bf923491b
Matthew McGuff <matthewmcguff85@gmail.com>
2018-02-02 15:56:22 -0700

    Adding sql file to see distinct conversations

---
+++ queries/distinct conversations.sql
@@ -0,0 +1,30 @@
+
+
+
+
+
+Declare @t table (id int identity, ConId nvarchar(max));
+
+insert into @t
+select distinct ConversationId
+from UserLog;
+
+Declare @i int = 1;
+while @i <= (select count(*) from @t)
+begin
+
+	select *
+	from UserLog
+	where ConversationId = 
+	(	select conId
+		from @t
+		where id = @i
+		
+	)
+
+	order by Created;
+
+	set @i += 1;
+end 
+
+
