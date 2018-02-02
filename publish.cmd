0962cb39756c7cc6d1051fbe256923c9849fa244
Matthew McGuff <matthewmcguff85@gmail.com>
2018-02-01 18:10:13 -0700

    Initial commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=mmcguffhw2-Web-Deploy.pubxml -p:Password=GaDYTGmkkRtk2gi8rmuEe2lhP5djjD45qPdxvHm4jaqfXEekDKcngwtldDE4
+
