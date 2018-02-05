6190699ad535e419e182236ecba7dee1580caa30
Matthew McGuff <matthewmcguff85@gmail.com>
2018-02-05 06:57:53 -0700

    I added my first if statement with dialog

--- Dialogs/EchoDialog.cs
+++ Dialogs/EchoDialog.cs
@@ -23,11 +23,30 @@ namespace Microsoft.Bot.Sample.SimpleEchoBot
 
         public async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> argument)
         {
-            var a = await argument as Activity;
-            
-                var response = "Repeating: " + a.Text;
+            var invite = "After centuries of being lost, the gospel of Jesus Christ has been restored to the earth by our loving Heavenly Father through a living prophet. The Book of Mormon is evidence of this. You can hold it in your hands. You can read it, ponder how the message in the book can improve your life, and pray to know that the message is the word of God.  I would like to offer your own personal copy of this book.Let me get some quick information from you:";
 
-                await context.PostAsync($"{response}");
+
+            var a = await argument as Activity;
+            var response = "";// = "Repeating: " + a.Text;
+
+            if (a.Text =="a")
+            {
+                response = "Response A";
+            }
+            else if (a.Text == "b")
+            {
+                response = "Response b";
+            }
+            else if (a.Text == "c")
+            {
+                response = "Response c";
+            }
+            else
+            {
+                response = "That is a good question!  I don't have a great answer for that but as Latter Days Saints we believe that all can receive answer from God (see Moroni: 10:3-5)";
+            }
+
+            await context.PostAsync($"{response}");
 
                 LogDatabase.WriteToDatabase
                  (
@@ -37,7 +56,20 @@ namespace Microsoft.Bot.Sample.SimpleEchoBot
                      , message: response
                  );
 
-                context.Wait(MessageReceivedAsync);            
+            await context.PostAsync($"{invite}");
+
+            //We might be able to get out of logging this information as it may make everything verbose
+            //It can be assurmed that the bot is going to say what its saying to the user perhaps.
+            //However so I can ensure it's doing whats its suppose to the logging stays. 
+            LogDatabase.WriteToDatabase
+            (
+                conversationid: a.Conversation.Id
+                , username: "ElderBot"
+                , channel: a.ChannelId
+                , message: response
+            );
+
+            context.Wait(MessageReceivedAsync);            
         }
 
         //public async Task AfterResetAsync(IDialogContext context, IAwaitable<bool> argument)
